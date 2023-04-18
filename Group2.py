a612208a5550f4745df6d720ba0eafed7c3bdd41
Youssef Gaber <youssefmohamedalyy@gmail.com>
2023-04-18 18:17:26 +0200

    Group2 Modifications

--- Group2.py
+++ Group2.py
@@ -58,8 +58,17 @@ class Page(QWidget):
 
       self.cal=QPushButton('Calculate Packet delay in-between selected ports')
       self.cal.clicked.connect(self.calcAvgDelay)
+      self.cal2=QPushButton('Calculate the Average cycle time of packets')
+      self.cal2.clicked.connect(self.calcCycleTime)
+      self.plot=QPushButton('View Plots')
+      self.plot.clicked.connect(self.viewPlots)
+
+      self.plot2=QPushButton('View Plots')
+      self.plot2.clicked.connect(self.viewPlots2)
       self.calcLabel=QLabel('Average Delay: ')
       self.calc=QLabel()
+      self.cycLabel=QLabel('Average Cycle Time: ')
+      self.avgCycleTime=QLabel()
       self.calc.setStyleSheet("background-color: white")
       self.clear=QPushButton('Clear current packets')
       self.addNew=QPushButton('Import New Packets')
@@ -74,9 +83,11 @@ class Page(QWidget):
       topLeft.addRow(srcLabel,self.source)
       topLeft.addRow(portA_Label,self.portA)
       topLeft.addRow(portB_Label,self.portB)
-      topRight.addRow(self.cal)
+      topRight.addRow(self.cal,self.plot)
       topRight.addRow(self.calcLabel,self.calc)
-      topRight.addRow(self.clear,self.addNew)
+      topRight.addRow(self.cal2,self.plot2)
+      topRight.addRow(self.cycLabel,self.avgCycleTime)
+      topLeft.addRow(self.clear,self.addNew)
       subMain.addLayout(topLeft)
       subMain.addLayout(topRight)
       #self.main2.addWidget(import_button2)
@@ -86,7 +97,7 @@ class Page(QWidget):
       window.tab2.setLayout(self.main2)
 
     def import_file(self):
-      global df
+      #global df
       file_path, _ = QFileDialog.getOpenFileName(self, "Select file", "", "CSV Files (*.csv);; Excel Files (*.xlsx)")
       if file_path.endswith('.csv'):
           self.df = pd.read_csv(file_path)
@@ -126,7 +137,7 @@ class Page(QWidget):
 
     def calcAvgDelay(self):
       #def cal(data,portA,portB):
-        data=self.df
+        data=self.df.copy()
         data.set_index('No.', inplace=True)
         df=data[data['Protocol'] == 'PNIO']
         src=self.source.currentText()
@@ -154,3 +165,103 @@ class Page(QWidget):
         self.avg=sum(val1) / len(val1)
         #print(self.avg)
         self.calc.setText(str(self.avg))
+        self.val=val1
+    
+    
+    def calcCycleTime(self):
+      
+        d=self.df.copy()
+        d.head(5)
+        d.set_index('No.', inplace=True)
+        d.head(5)
+        prtA=self.portA.currentText()
+        datax=d[d['Reception Port'] == int(prtA)]
+        datax.head(5)
+        count=datax['Time'].count()
+        #print(count)
+        
+        flag=1
+        deltas=[]
+        
+        for i in range(0,count):
+            #print(datax.iloc[[i],[3]].values)
+            if ((datax.iloc[[i],[3]].values == 'PNIO') & (flag==1)):
+                time1=datax.iloc[[i],[0]].values
+                flag=0
+                #print(time1)
+            elif ((datax.iloc[[i],[3]].values == 'PNIO') & (flag==0)):
+                time2=datax.iloc[[i],[0]].values
+                delta=time2[0]-time1[0]
+                #print(time2)
+                deltas.append(delta[0])
+                flag=1
+        
+        #print(deltas[0:5])
+        avgCycleTime=sum(deltas) / len(deltas)
+        self.avgCycleTime.setText(str(avgCycleTime))
+        self.deltas=deltas
+        #print(self.deltas)
+        
+    def viewPlots(self):
+        fig = plt.figure(figsize=(8, 6))
+        x=len(self.val)
+        plt.plot(range(0,x), self.val,'b')
+        plt.xlabel('Packet')
+        plt.ylabel('Packet Delay')
+        plt.title('Delay time of packets within the test setup ')
+        graph_window = QDialog(self)
+        graph_window.setWindowTitle('Graphs')
+        # Embed the graph in the Qt window using FigureCanvasQTAgg
+        canvas = FigureCanvasQTAgg(fig)
+
+
+        fig2=plt.figure(figsize=(8,6))
+        plt.boxplot(self.val)
+        plt.xlabel('Data Capture')
+        plt.ylabel('Packet Delay')
+        plt.title('Delay of packets Box Plots ')
+        canvas2=FigureCanvasQTAgg(fig2)
+        graph_window.setLayout(QVBoxLayout())
+        graph_window.layout().addWidget(canvas)
+        graph_window.layout().addWidget(canvas2)
+
+
+        #graph_window.layout().addWidget(AvgLabel)
+
+        # Show the window
+        graph_window.exec_()
+        
+        # Close the previous figure to avoid displaying it in the Python output
+        plt.close(fig)
+
+    def viewPlots2(self):
+        fig = plt.figure(figsize=(8, 6))
+        x=len(self.deltas)
+        plt.plot(range(0,x), self.deltas,'b')
+        plt.xlabel('Packet')
+        plt.ylabel('Cycle Time')
+        plt.title('cycle time of packets ')
+        graph_window = QDialog(self)
+        graph_window.setWindowTitle('Graphs')
+        # Embed the graph in the Qt window using FigureCanvasQTAgg
+        canvas = FigureCanvasQTAgg(fig)
+
+
+        fig2=plt.figure(figsize=(8,6))
+        plt.boxplot(self.deltas)
+        plt.xlabel('Data Capture')
+        plt.ylabel('Cycle Time')
+        plt.title('Cycle Time Box Plot ')
+        canvas2=FigureCanvasQTAgg(fig2)
+        graph_window.setLayout(QVBoxLayout())
+        graph_window.layout().addWidget(canvas)
+        graph_window.layout().addWidget(canvas2)
+
+
+        #graph_window.layout().addWidget(AvgLabel)
+
+        # Show the window
+        graph_window.exec_()
+        
+        # Close the previous figure to avoid displaying it in the Python output
+        plt.close(fig)
